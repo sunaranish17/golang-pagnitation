2eb209b8e1429e8414164012eb96a68d2ddcf09e
Anish <sunaranish17@gmail.com>
2021-01-14 14:15:49 +0545

    COnfig file added

---
+++ src/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+//DBConfig represents db configuration
+type DBConfig struct {
+	Host     string
+	Port     string
+	User     string
+	DBName   string
+	Password string
+}
+
+func BuildDBConfig() *DBConfig {
+	dbConfig := DBConfig{
+		Host:     "localhost",
+		Port:     "3306",
+		User:     "root",
+		Password: "admin123",
+		DBName:   "golang-pagination",
+	}
+	return &dbConfig
+}
+
+func DbURL(dbConfig *DBConfig) string {
+	return fmt.Sprintf(
+		"%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local",
+		dbConfig.User,
+		dbConfig.Password,
+		dbConfig.Host,
+		dbConfig.Port,
+		dbConfig.DBName,
+	)
+}
