f4b40dcd78bd77705fb75ab4ab8e4a6b4c50421e
Anish <sunaranish17@gmail.com>
2021-01-14 17:37:05 +0545

    User repo created

---
+++ src/repo/user.go
@@ -0,0 +1,18 @@
+package repo
+
+import (
+	"golang-pagination/src/config"
+	"golang-pagination/src/models"
+)
+
+func GetAllUsers(user *models.User, pagination *models.Pagination) (*[]models.User, error) {
+	var users []models.User
+	offset := (pagination.Page - 1) * pagination.Limit
+	queryBuilder := config.DB.Limit(pagination.Limit).Offset(offset).Order(pagination.Sort)
+	result := queryBuilder.Model(&models.User{}).Where(user).Find(&users)
+	if result.Error != nil {
+		msg := result.Error
+		return nil, msg
+	}
+	return &users, nil
+}
